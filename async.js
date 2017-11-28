a1b71dd7f6afc58142bddfc99680e25e1d09ce9b
Ганиева Альбина Нажиповна <erfolg1996@mail.ru>
2017-11-28 23:54:33 +0500

    решение задачи

--- async.js
+++ async.js
@@ -9,5 +9,31 @@ exports.runParallel = runParallel;
  * @param {Number} timeout - таймаут работы промиса
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+
+    return new Promise(resolve => {
+        if (!jobs.length) {
+            resolve([]);
+        }
+        let answers = [];
+        let next = 0;
+        for(let i = 0; i < parallelNum; i++) {
+            next = i + parallelNum - 1;
+            startPromise(i, next);
+        }
+        function startPromise(index, next) {
+            let finish =  function () {
+                answers[index] = arguments['0'];
+                if (answers.length === jobs.length) {
+                    resolve(answers);
+                } 
+                if (answers.length < jobs.length) {
+                    startPromise(next++, next++);
+                }
+            };
+            new Promise((resolve, reject) => {
+                jobs[index]().then(resolve, reject);
+                setTimeout(reject, timeout, new Error('Promise timeout'));
+            }).then(finish).catch(finish);
+        }
+    });
 }
